5dca1f7adb2be080b704285dfaf2a7921beda5e1
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2022-08-05 14:51:55 +0530

    :deploy: create release

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,3 @@ fn main() -> Result<()> {
 
     Ok(())
 }
-
-#[test]
-fn find_a_match() {
-    let mut result = Vec::new();
-    grrs::find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
-    assert_eq!(result, b"lorem ipsum\n");
-}
